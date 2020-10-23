1b95fe6a82c126325ccba16b2b5ed2f09c125f23
faiqdhimas <faiq.dhms@gmail.com>
2020-10-23 11:42:57 +0700

    Coba nambah RayhanActivity :))

---
+++ app/src/main/java/com/teamfaiq/restoapp/RayhanActivity.java
@@ -0,0 +1,15 @@
+package com.teamfaiq.restoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RayhanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rayhan);
+    }
+//    INI ACTIVITYNYA RAYHAN.. JGN UBAH2 DISINI YEEE !!
+}
