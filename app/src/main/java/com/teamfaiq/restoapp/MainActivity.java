94d72d293e8affc488e29959fbd75562c29d2ccc
Rayhan Mahardhika Wijaya <rayhanmahardhika2000@gmail.com>
2020-10-25 07:32:46 +0700

    Add intent button in MainActivity.java

--- app/src/main/java/com/teamfaiq/restoapp/MainActivity.java
+++ app/src/main/java/com/teamfaiq/restoapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.teamfaiq.restoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toPesanan(View view) {
+        Intent intent = new Intent(this, RayhanActivity.class);
+        startActivity(intent);
     }
 }
